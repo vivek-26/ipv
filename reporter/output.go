993bb4d8b9d89621cde4b1198d1be2d3fcbb53a3
Vivek Kumar <vivek.kumar26@live.com>
2019-08-12 17:33:58 +0530

    add success reporter func

--- reporter/output.go
+++ reporter/output.go
@@ -17,6 +17,11 @@ func Warn(msg interface{}) {
 	fmt.Println(au.BrightYellow(msg).Bold())
 }
 
+// Success prints success messages to console
+func Success(msg interface{}) {
+	fmt.Println(au.BrightGreen(msg).Bold())
+}
+
 // Error prints error messages to console
 func Error(msg interface{}) {
 	fmt.Println("Error: ", au.BrightRed(msg).Bold())
