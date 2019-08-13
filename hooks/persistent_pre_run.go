33fe995fcab1268b76f3be5c82956046c5afab7d
Vivek Kumar <vivek.kumar26@live.com>
2019-08-13 21:18:04 +0530

    change messages

--- hooks/persistent_pre_run.go
+++ hooks/persistent_pre_run.go
@@ -9,12 +9,12 @@ import (
 	"github.com/vivek-26/ipv/reporter"
 )
 
-const targetIP = "8.8.8.8" // Google DNS IP
-const nPing = 3            // Number of times to ping
+const targetIP = "8.8.8.8" // Google DNS Server IP
+const nPing = 5            // Number of times to ping
 
-// PersistentPreRun performs DNS cehcking
+// PersistentPreRun performs internet connection check
 func PersistentPreRun(cmd *cobra.Command, args []string) {
-	reporter.Info("Checking DNS connection...")
+	reporter.Info("Checking internet connection...")
 	pinger, err := ping.NewPinger(targetIP)
 	if err != nil {
 		reporter.Error(err)
@@ -31,8 +31,8 @@ func PersistentPreRun(cmd *cobra.Command, args []string) {
 	// 1) `stats.PacketLoss` is NaN
 	// 2) Average round trip time (stats.AvgRtt) is 0
 	if math.IsNaN(stats.PacketLoss) || stats.AvgRtt == 0 {
-		reporter.Error("DNS check failed ✗")
+		reporter.Error("Internet connection check failed ✗")
 	}
 
-	reporter.Success("DNS check successful ✓")
+	reporter.Success("Internet connection check successful ✓")
 }
