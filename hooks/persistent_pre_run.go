3bdf8ccb917e49cbe3c3564d32630f1275193149
Vivek Kumar <vivek.kumar26@live.com>
2019-08-16 10:49:30 +0530

    change icmp ping lib

--- hooks/persistent_pre_run.go
+++ hooks/persistent_pre_run.go
@@ -1,38 +1,66 @@
 package hooks
 
 import (
-	"math"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	ping "github.com/sparrc/go-ping"
 	"github.com/spf13/cobra"
+	"github.com/tatsushid/go-fastping"
 
 	"github.com/vivek-26/ipv/reporter"
 )
 
-const targetIP = "8.8.8.8" // Google DNS Server IP
-const nPing = 5            // Number of times to ping
+const targetAddr = "ipvanish.com"
+const maxRTT = time.Second * 2 // Max round trip time
 
 // PersistentPreRun performs internet connection check
 func PersistentPreRun(cmd *cobra.Command, args []string) {
 	reporter.Info("Checking internet connection...")
-	pinger, err := ping.NewPinger(targetIP)
+	p := fastping.NewPinger()
+	p.MaxRTT = maxRTT
+
+	ra, err := net.ResolveIPAddr("ip4:icmp", targetAddr)
 	if err != nil {
 		reporter.Error(err)
 	}
 
-	pinger.Count = nPing
-	pinger.SetPrivileged(true)
-	pinger.Run() // Blocks until finished
-	stats := pinger.Statistics()
+	// Add target IP address
+	p.AddIPAddr(ra)
 
-	// `pinger.Run()` does not return errors. Hence, to know if an error
-	// has occurred, we use `pinger.Statistics()`.
-	// To determine errors, two conditions are used -
-	// 1) `stats.PacketLoss` is NaN
-	// 2) Average round trip time (stats.AvgRtt) is 0
-	if math.IsNaN(stats.PacketLoss) || stats.AvgRtt == 0 {
-		reporter.Error("Internet connection check failed ✗")
+	onRecv, onIdle := make(chan struct{}), make(chan struct{})
+
+	// Received ICMP message handler
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		onRecv <- struct{}{}
 	}
 
-	reporter.Success("Internet connection check successful ✓")
+	// Max RTT expiration handler
+	p.OnIdle = func() {
+		onIdle <- struct{}{}
+	}
+
+	p.RunLoop() // Non blocking
+
+	c := make(chan os.Signal, 1) // Look for `ctrl-c`
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+
+	select {
+	case <-c:
+		reporter.Error("Process stopped by user")
+	case <-onRecv:
+		reporter.Success("Internet connection check successful ✓")
+	case <-onIdle:
+		reporter.Error("Internet connection check failed ✗")
+	case <-p.Done():
+		if err := p.Err(); err != nil {
+			reporter.Error(err)
+		}
+	}
+
+	signal.Stop(c)
+	p.Stop()
 }
