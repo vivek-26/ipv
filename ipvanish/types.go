a2467bef2fa78ef9d2049158da54d539bf280c70
Vivek Kumar <vivek.kumar26@live.com>
2019-08-18 14:52:41 +0530

    implement sort.Interface & Stringer interface

--- ipvanish/types.go
+++ ipvanish/types.go
@@ -1,6 +1,7 @@
 package ipvanish
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -9,6 +10,26 @@ type IPVServer struct {
 	IP       string
 	Hostname string
 	Latency  time.Duration
+}
+
+// ByLatency implements sort.Interface based on Latency field
+type ByLatency []IPVServer
+
+// Len returns number of servers
+func (s ByLatency) Len() int { return len(s) }
+
+// Less compares latency of two servers
+func (s ByLatency) Less(i, j int) bool { return s[i].Latency < s[j].Latency }
+
+// Swap exchanges servers to order them
+func (s ByLatency) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+// Implement stringer interface
+func (i IPVServer) String() string {
+	return fmt.Sprintf(
+		"Host: %v, IP: %v, Latency: %v",
+		i.Hostname, i.IP, i.Latency,
+	)
 }
 
 // ClientInfo represents current connection info of user
