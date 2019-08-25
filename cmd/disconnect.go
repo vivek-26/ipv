ae9de55c02195d4c7d7952805627556d7de10d78
Vivek Kumar <vivek.kumar26@live.com>
2019-08-25 10:33:52 +0530

    disconnect from vpn

--- cmd/disconnect.go
+++ cmd/disconnect.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/vivek-26/ipv/ipvanish"
 )
 
 // disconnectCmd represents disconnect command
@@ -10,6 +11,9 @@ func disconnectCmd() *cobra.Command {
 		Use:   "disconnect",
 		Short: "Disconnects from VPN by killing VPN process",
 		Long:  "  Disconnects from VPN by killing VPN process",
-		Run:   func(cmd *cobra.Command, args []string) {},
+		Run: func(cmd *cobra.Command, args []string) {
+			// Disconnect from vpn
+			ipvanish.Disconnect()
+		},
 	}
 }
