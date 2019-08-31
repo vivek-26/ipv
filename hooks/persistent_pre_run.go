9f7e78f49e7a50154436a61fe781e51c1b46d2c3
Vivek Kumar <vivek.kumar26@live.com>
2019-08-31 15:00:51 +0530

    check for active vpn process prior to connect

--- hooks/persistent_pre_run.go
+++ hooks/persistent_pre_run.go
@@ -2,11 +2,14 @@ package hooks
 
 import (
 	"bytes"
+	"fmt"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
 
+	"github.com/vivek-26/ipv/ipvanish"
 	"github.com/vivek-26/ipv/reporter"
 )
 
@@ -16,6 +19,19 @@ const maxRTT = time.Second * 1 // Max round trip time
 // PersistentPreRun checks for openvpn binary
 func PersistentPreRun(cmd *cobra.Command, args []string) {
 	checkOpenvpnBinary()
+
+	// Check if a vpn process is already running, if true, then abort
+	isRunning, pid, _ := ipvanish.IsVPNProcessRunning()
+	if isRunning {
+		reporter.Error(
+			strings.Join(
+				[]string{
+					fmt.Sprintf("A VPN process with PID %v is already running.", pid),
+					"Please disconnect using command `sudo ipv disconnect`",
+				},
+				"\n"),
+		)
+	}
 }
 
 // checkOpenvpnBinary makes sure that openvpn binary is installed on the system
