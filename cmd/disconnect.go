fff16ecce0d0b688835198f6de403d78705d55b3
Vivek Kumar <vivek.kumar26@live.com>
2019-09-01 15:27:21 +0530

    indent help text

--- cmd/disconnect.go
+++ cmd/disconnect.go
@@ -3,14 +3,15 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"github.com/vivek-26/ipv/ipvanish"
+	"github.com/vivek-26/ipv/utils"
 )
 
 // disconnectCmd represents disconnect command
 func disconnectCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "disconnect",
-		Short: "Disconnects from VPN by killing VPN process",
-		Long:  "  Disconnects from VPN by killing VPN process",
+		Short: "Disconnects from VPN by killing the OpenVPN process",
+		Long:  utils.HelpText("Disconnects from VPN by killing the OpenVPN process"),
 		Run: func(cmd *cobra.Command, args []string) {
 			// Disconnect from vpn
 			ipvanish.Disconnect()
